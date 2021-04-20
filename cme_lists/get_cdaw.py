43e2edfa4997cc93739c61e169976ab3dee3c17a
Laura Hayes <hayesla@tcd.ie>
2021-04-20 18:36:16 +0100

    final_run

--- cme_lists/get_cdaw.py
+++ cme_lists/get_cdaw.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import urllib
+import random
 
 def get_cdaw_file():
 	"""
@@ -56,5 +57,108 @@ def cdaw_to_df(file):
 
 cdaw_data = cdaw_to_df("univ_all.txt")
 cdaw_data["time_c2"] = pd.to_datetime(cdaw_data.date + cdaw_data.time, format="%Y/%m/%d%H:%M:%S")
+
+# lets trim this list to the last solar cycle as that's what I'm interested in.
+cdaw_data_final = cdaw_data[(cdaw_data["time_c2"]>=("2010-01-01")) & (cdaw_data["time_c2"]<="2018-12-31")]
+cdaw_data_final.reset_index(inplace=True, drop=True)
+
+
+def get_event_txtfile(cme_event):
+	"""
+	Construct txt file urls based on particular pattern.
+	Bit messy but works.
+
+	The files follow the format:
+	 yyyymmdd.hhmmss.w(width){n,p,h}.v{velocity}.p(position_angle)g.yht
+
+	Parameters
+	----------
+	cme_event : pd.Series
+		row of the cdaw pandas dataframe
+
+	Returns
+	-------
+	final_url : `str` - the corresponding url to cme_event input.
+
+	"""	
+	# cme_event = cdaw_data_final.iloc[i]
+
+	urlbase = "https://cdaw.gsfc.nasa.gov/CME_list/UNIVERSAL/"
+	
+	if cme_event["central_pa"]=="Halo":
+		type_cme = "halo"
+	elif "Partial Halo" in cme_event["remarks"]:
+		type_cme = "partial_halo"
+	else:
+		type_cme = "normal"
+
+	dict_type = {"halo":"h", "partial_halo": "p", "normal":"n"}
+	final_url = urlbase + cme_event["time_c2"].strftime("%Y_%m/yht/%Y%m%d.%H%M%S") \
+	             + ".w{:03d}{:s}".format(int(cme_event["width"]), dict_type[type_cme]) \
+	             + ".v{:04d}".format(int(cme_event["linear_speed"])) \
+	             + ".p{:03d}g.yht".format(int(cme_event["MPA"]))
+	return final_url
+
+def read_event_txtfile(url):
+	opn = urllib.request.urlopen(url)
+	lines = [x.decode() for x in opn.readlines()]
+	for l in lines:
+		if "QUALITY_INDEX" in l:
+			return  " ".join(l.split())
+
+def get_quality(save=False):
+	"""
+	Read quality index from associated event txt files and include to cdaw list.
+
+	Takes an age and a half, definitely a cleaner to do it but this will do for now.
+	"""
+	import time
+	t1 = time.time()
+	quality_index = []
+	for i in range(len(cdaw_data_final)):
+		print(100*(i/len(cdaw_data_final)))
+		txt_url = get_event_txtfile(cdaw_data_final.iloc[i])
+		try:
+			qual = read_event_txtfile(txt_url)
+		except:
+			qual=""
+			print("couldn't read url")
+		quality_index.append(qual)
+	t2 = time.time()
+	print(t2 - t1)
+
+	cdaw_data_final["quality_index"] = quality_index
+	if save:
+		cdaw_data_final.to_csv("cdaw_2010_2018_w_quality.csv", index_label=False)
+
+
+
+#### tests ##################
+def check_url_exists(url):
+	try:
+		urllib.request.urlopen(url).getcode()
+	except urllib.error.HTTPError as e:
+		print('HTTPError: {}'.format(e.code))
+		return "error!"
+
+def random_test_of_urls():
+	all_urls = [get_event_txtfile(cdaw_data_final.iloc[i]) for i in range(len(cdaw_data_final))]
+	errors = []
+	for i in range(100):
+		ind = random.randrange(0, len(all_urls))
+		aa = check_url_exists(all_urls[ind])
+		if aa == "error!":
+			errors.append(ind)
+	print(errors)
+
+
+
+
+
+
+
+
+
+
 
 
