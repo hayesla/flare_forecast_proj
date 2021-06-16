97463f13cc853c68ca80f004b7748f7eb9a2869a
Laura Hayes <hayesla@tcd.ie>
2021-06-16 14:21:24 +0100

    now standalone

--- goes_flare_list/swpc_report_flarelist.py
+++ goes_flare_list/swpc_report_flarelist.py
@@ -9,36 +9,47 @@ from dateutil.relativedelta import relativedelta
 import pandas as pd 
 import datetime
 
-def get_yearly_tar_files():
+
+def get_yearly_tar_files(tstart, tend, savedir='./goes_files'):
 	"""
-	Download yearly tar files that contain all the daily swpc reports and
-	save them to the `goes_files` dir.
+	Function to download yearly tar files that contain all the daily swpc reports and
+	save them to the `./goes_files` dir (by default).
+
+	Parameters
+	----------
+	tstart : ~str
+		start date of search to download
+	tend : ~str
+		end date of search to download
+	savedir : ~str, optional
+		path to where to save the files. Default is ./goes_files.
+
 	"""
 	file_url = "ftp://ftp.swpc.noaa.gov/pub//warehouse/%Y/%Y_events.tar.gz"
-	scraper = scraper.Scraper(file_url)
+	goes_scraper = scraper.Scraper(file_url)
 
-	urls = scraper.filelist(TimeRange("2010-01-01", "2018-01-01"))
+	urls = goes_scraper.filelist(TimeRange(tstart, tend))
 
 	for u in urls:
 		urllib.request.urlretrieve(u, u.split("/")[-1])
 
-
 	tar_files = glob.glob("*event*.tar.gz")
 	for f in tar_files:
 		my_tar = tarfile.open(f)
-		my_tar.extractall('./goes_files') # specify which folder to extract to
+		my_tar.extractall(savedir) # specify which folder to extract to
 		my_tar.close()
 
 
-def get_swpc_flarelist():
+def get_swpc_flarelist(tstart, tend, csv_filename='swpc_event_list.csv'):
 	"""
 	Function to read in all SWPC daily reports and save flarelist
 	as a csv for flares >=C1.0.
 
 	The flarelist is saved to "swpc_event_list.csv"
+
 	"""
 	filedir = "./goes_files/%Y_events/%Y%m%devents.txt"
-	timerange = TimeRange("2010-01-01", "2018-12-31")
+	timerange = TimeRange(tstart, tend)
 	t0 = timerange.start.datetime
 	files = [t0.strftime(filedir)]
 	while timerange.end.datetime>t0:
@@ -57,66 +68,17 @@ def get_swpc_flarelist():
 
 	df_flares_c = df_flares[df_flares["goes_class_ind"].isin(["C", "X", "M"])]
 	df_flares_c.reset_index(inplace=True, drop=True)
-	df_flares_c.to_csv("swpc_event_list.csv", index_label=False)
+	df_flares_c.to_csv(csv_filename, index_label=False)
 
-def test_files_missing():
-	"""
-	Print missing files - if any missing check solarmoniter.
-	"""
-	filedir = "./goes_files/%Y_events/%Y%m%devents.txt"
-	t0 = timerange.start.datetime
-	days = [t0]
-	while timerange.end.datetime > t0:
-		t0 = t0 + relativedelta(days=1)
-		days.append(t0)
+"""
+--------------------------------------------------------
+This is where you would add in your own timerange dates, 
+your own local directory to save the daily report txt files
+and define the name you want for your output csv to save 
+flarelist.
+--------------------------------------------------------
+"""
 
-	missing_files = []
-	for d in days:
-		if not os.path.exists(d.strftime(filedir)):
-			missing_files.append(d.strftime(filedir))
-	print(missing_files)
-
-
-
-## Testing differences
-
-def get_yearly_swpc(year):
-	all_files = []
-	all_files += glob.glob(year.strftime(filedir))
-	all_files.sort()
-	df_flares = read_swpc_reports(all_files[0])
-	for f in all_files[1:]:
-		df = read_swpc_reports(f)
-		df_flares = df_flares.append(df)
-	df_flares.reset_index(inplace=True, drop=True)
-	df_flares["ts"] = df_flares.date + df_flares.start_time
-	df_flares.drop_duplicates(subset="ts")
-	return df_flares[df_flares["goes_class_ind"].isin(["C", "X", "M"])]
-
-
-def get_yearly_ngdc(year):
-	file = "goes-xrs-report_%Y.txt"
-	df = read_ngdc_goes_reports(year.strftime(file))
-	df["ts"] = df.date + df.start_time
-	df.drop_duplicates(subset="ts")
-	return df[df["goes_class_ind"].isin(["X", "M", "C"])]
-
-
-def print_flares(df):
-	x = np.sum(df["goes_class_ind"].isin(["X"]))
-	m = np.sum(df["goes_class_ind"].isin(["M"]))
-	c = np.sum(df["goes_class_ind"].isin(["C"]))	
-	print("X: {:d}, M: {:d}, C: {:d}".format(x, m, c))
-
-
-df_swpc_2012 = get_yearly_swpc(datetime.datetime(2012,1,1))
-df_ngdc_2012 = get_yearly_ngdc(datetime.datetime(2012,1,1))
-
-extra_flare_times = list((set(df_ngdc_2012["ts"]) - set(df_swpc_2012["ts"])))
-extra_flare_times = list((set(df_swpc_2012["ts"]) - set(df_ngdc_2012["ts"])))
-
-def print_dif(year):
-	print("SWPC:")
-	print_flares(get_yearly_swpc(datetime.datetime(year,1,1)))
-	print("NGDC:")
-	print_flares(get_yearly_ngdc(datetime.datetime(year,1,1)))
+# tstart, tend = "2010-01-01", "2018-01-01"
+# get_yearly_tar_files(tstart, tend)
+# get_swpc_flarelist(tstart, tend)
