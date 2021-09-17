a806c81c395216636cd77fc61be560e348065bae
Laura Hayes <hayesla@tcd.ie>
2021-09-17 16:36:15 +0100

    adding comments

--- forecast_tests/metric_utils.py
+++ forecast_tests/metric_utils.py
@@ -7,16 +7,16 @@ from sklearn.calibration import calibration_curve
 
 ############## SKILL SCORES #################
 
-def get_tss(Y_test, prediction):
+def calculate_tss(true_vals, pred_vals):
     """
     Get the True Skill Score (TSS) to test the overall predictive 
     abilities of a given forecast.
 
     Parameters
     ----------
-    Y_test : `~np.array`
+    true_vals : `~np.array`
         The true Y (label) values. The test outputs to compare with the predicted values
-    prediction : `~np.array`
+    pred_vals : `~np.array`
         The predicted Y values from the model.
 
     Returns
@@ -29,13 +29,13 @@ def get_tss(Y_test, prediction):
     See Bloomfield et al. 2012.
 
     """
-    tn, fp, fn, tp = metrics.confusion_matrix(Y_test, prediction).ravel()
+    tn, fp, fn, tp = metrics.confusion_matrix(true_vals, pred_vals).ravel()
     
     TSS = (tp / (tp + fn)) - (fp / (fp + tn))
     return TSS
 
 
-def get_tss_threshold(true_vals, pred_vals, thresh):
+def calculate_tss_threshold(true_vals, prob_vals, thresh):
     """
     This will get the TSS for a given threshold. This should be 
     used when the forecast gives a probability
@@ -45,28 +45,28 @@ def get_tss_threshold(true_vals, pred_vals, thresh):
     ---------
     true_values: `~np.array`
         the true values.
-    pred_values: `~np.array`
+    prob_vals: `~np.array`
         the predicted value probabilities.
     thresh : `~float`
         the threshold value to take for binary event (i.e. values above this 
         threshold are taken to be 1 (flare) and those below as 0 (no flare)).
     """
-    pred_y_thresh = [1 if x>thresh else 0 for x in pred_vals]
-    tn, fp, fn, tp = confusion_matrix(true_vals, pred_y_thresh).ravel()
+    pred_thresh = [1 if x>thresh else 0 for x in prob_vals]
+    tn, fp, fn, tp = metrics.confusion_matrix(true_vals, pred_thresh).ravel()
     TSS = (tp / (tp + fn)) - (fp / (fp + tn))
     return TSS
 
 
-def get_bss(Y_test, prob_pos):
+def calculate_bss(true_vals, prob_vals):
     """
     Get the Brier Skill Score (BSS) of a predictive model.
 
     Parameters
     ----------
     Y_test : `~np.array`
-        The true Y (label) values. The test outputs to compare with the predicted values
-    prediction : `~np.array`
-        The predicted probability values of the model. Typically gotten from mdl.predict_proba() function.
+        The true Y (label) values. The test outputs to compare with the predicted values.
+    prob_vals : `~np.array`
+        The predicted probability values of the model. If using sklearn model - use mdl.predict_proba() function.
 
     Returns
     -------
@@ -74,60 +74,110 @@ def get_bss(Y_test, prob_pos):
         calculated BSS
 
     """
-    bs = metrics.brier_score_loss(Y_test, prob_pos)
-    bs_clim = np.mean(Y_test)
+    bs = metrics.brier_score_loss(true_vals, prob_vals)
+    bs_clim = np.mean(true_vals)
     BSS = 1 - bs/bs_clim
     return BSS
+
 
 
 ############## FORECAST METRIC PLOTS #################
 
 
-def plot_roc_and_reliability_curve(mdl, X_test, Y_test, ):
+def plot_roc_curve(true_vals, prob_vals, ax=None):
     """
+    Plot the receiver operating characteristic (ROC) curve
+
     Parameters
     ----------
-    mdl : sklearn model that has been already fit
-    features : `pd.DataFrame` of features. 
-    X_test : `~np.array`
-        The true test feature values. The test inputs to the mdl. 
-    Y_test : `~np.array`
-        The true test Y (label) values. The test outputs to compare with the predicted values.
+    true_vals : `~np.array`
+        The true Y (label) values. The test outputs to compare with the predicted values.
+    prob_vals : `~np.array`
+        The predicted probability values of the model.
+    ax : `~matplotlib.axes.Axes`, optional
+        If provided the image will be plotted on the given axes.
+    
+    Returns
+    -------
+    Plots the ROC curve
 
+    ax : ~`matplotlib axes`
+    """
+    fpr, tpr, _ = metrics.roc_curve(true_vals, prob_vals)
+    auc_mcstat = metrics.auc(fpr, tpr)
+    climatology = np.mean(true_vals)
+    
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax.plot(fpr, tpr, label="(AUC = {:.2f})".format(auc_mcstat))
+    ax.plot([0, 1], [0, 1], color='grey', linestyle='--')
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.legend(loc="lower right")
+    ax.set_title("ROC Curve")
+    plt.show()
+
+
+
+def plot_reliability_curve(true_vals, pred_vals, n_bins=10):
+    """
+    Plot the reliability curve (also known as a calibration curve).
+
+    Parameters
+    ----------
+    true_vals : `~np.array`
+        The true Y (label) values. The test outputs to compare with the predicted values
+    pred_vals : `~np.array`
+        The predicted Y values from the model.
+    n_bins : `int`, optional, default 10.
+        Number of bins to discretize the [0, 1] interval (input to sklearn `calibration_curve` function).
+    
+    Returns
+    -------
+    Plots the reliability diagram
+
+    ax : ~`matplotlib axes`
+    
     """
 
-    if hasattr(mdl, "predict_proba"):
-        prob_pos = mdl.predict_proba(X_test)[:, 1]
-    else: 
-        prob_pos = mdl.decision_function(X_test)
-        prob_pos = \
-            (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())
+    fraction_of_positives, mean_predicted_value = calibration_curve(true_vals, pred_vals, n_bins=n_bins)
+    climatology = np.mean(true_vals)
 
-    fraction_of_positives, mean_predicted_value = \
-        calibration_curve(Y_test, prob_pos, n_bins=10)
+    fig = plt.figure(figsize=(6,6))
+    gs1 = fig.add_gridspec(nrows=4, ncols=1)
+    ax1 = fig.add_subplot(gs1[0:3, 0])
+    ax2 = fig.add_subplot(gs1[3, 0], sharex=ax1)
 
 
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    ax1.plot(mean_predicted_value, fraction_of_positives, "s-")
+    
+    ax1.set_ylabel("Fraction of positives")
+    ax1.tick_params(which="both", labelbottom=False)
+    ax1.axhline(climatology, color="grey", label="climatology")
+    ax1.legend()
+    ax1.set_title("Reliability Curve")
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(11, 5))
-    # plot ROC curve
-    metrics.plot_roc_curve(mdl, X_test, Y_test, drawstyle="steps-mid", ax=ax1)
-    # plot reliability diagram
-    ax2.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
-    ax2.plot(mean_predicted_value, fraction_of_positives, "s-")
-    ax2.set_ylabel("Fraction of positives")
+    ax2.hist(pred_vals, range=(0, 1), bins=10, 
+             histtype="step", lw=2)
     ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("# events")
 
     plt.tight_layout()
+    plt.subplots_adjust(hspace=0.05)
+    plt.show()
+
 
 def plot_feature_importance(mdl, features, top=None, title="Feature importance"):
     """
-    Function to plot the importance of features from a model. 
+    Function to plot the importance of features from a sklearn model. 
     
     Parameters
     ----------
     mdl : sklearn model that has been already fit
     features : `pd.DataFrame` of features. 
-    top : `int`, optional, number of top features to plot.
+    top : `int`, number of top features to plot, optional.
           default is to plot all. 
     
     """
